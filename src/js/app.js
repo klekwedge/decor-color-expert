e934085108611c11c0bbd427bb229b744059330b
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-18 14:07:05 +0300

    refactor: change structure of the project

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 "use strict";
 
-import burger from "./modules/burger";
-import animationScroll from "./modules/animation-scroll";
+import burger from "./modules/burger.js";
+import animationScroll from "./modules/animation-scroll.js";
 
 window.addEventListener("DOMContentLoaded", () => {
   burger();
