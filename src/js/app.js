a833df699ef02d2c35c4f2564805f97fa9cfb350
klekkwedge <antonov.andrey.10101@gmail.com>
2022-05-18 14:10:29 +0300

    build: update deploy

--- src/js/app.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-import burger from "./modules/burger.js";
-import animationScroll from "./modules/animation-scroll.js";
-
-window.addEventListener("DOMContentLoaded", () => {
-  burger();
-  animationScroll();
-});
